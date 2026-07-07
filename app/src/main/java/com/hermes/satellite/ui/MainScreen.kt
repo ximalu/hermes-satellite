a20559f6882e59e2cedb167167abf16ff371da15
Ubuntu <ubuntu@localhost.localdomain>
2026-07-07 22:31:33 +0800

    Fix: add missing dp import in MainScreen

--- app/src/main/java/com/hermes/satellite/ui/MainScreen.kt
+++ app/src/main/java/com/hermes/satellite/ui/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 private data class NavTab(val label: String, val icon: ImageVector)
 
